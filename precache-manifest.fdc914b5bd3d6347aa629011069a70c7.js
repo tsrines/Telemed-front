1b72259de7e3fd2282e1371c0bf29eb723c5491a
Rines.io <tsrines@gmail.com>
2020-05-06 06:22:22 -0400

    Updates

---
+++ precache-manifest.fdc914b5bd3d6347aa629011069a70c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59d5fbbf9fbf64051869b698478b218",
+    "url": "/Telemed-front/index.html"
+  },
+  {
+    "revision": "8e3971a00345199ab733",
+    "url": "/Telemed-front/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "cf3adbf4fd089643764f",
+    "url": "/Telemed-front/static/js/2.73bc9ded.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/Telemed-front/static/js/2.73bc9ded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e3971a00345199ab733",
+    "url": "/Telemed-front/static/js/main.57b5a8ef.chunk.js"
+  },
+  {
+    "revision": "dbea78ed63df2447eb64",
+    "url": "/Telemed-front/static/js/runtime-main.c3c5198c.js"
+  }
+]);
