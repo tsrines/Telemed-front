6daa4275727c91a14dcab0f594d3b21225fac8ec
Rines.io <tsrines@gmail.com>
2020-05-20 15:54:25 -0400

    scripts

---
+++ scripts/create-env.js
@@ -0,0 +1,4 @@
+const fs = require('fs')
+fs.writeFileSync('./.env', `URL=${process.env.URL}\n`)
+fs.writeFileSync('./.env', `REACT_APP_GOOGLE_GEOCODE_API_KEY=${process.env.REACT_APP_GOOGLE_GEOCODE_API_KEY}\n`)
+fs.writeFileSync('./.env', `REACT_APP_BETTER_DOC_API_KEY=${process.env.REACT_APP_BETTER_DOC_API_KEY}\n`)
